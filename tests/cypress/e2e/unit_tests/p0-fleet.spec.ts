988fd7042d12ffa2f797e9e995a6f28cfcdcd6aa
Manuel Martín <manuel.martin@suse.com>
2024-02-02 17:34:30 +0100

    Adding new p0 spec and moving test from menu Finishing test case fleet 62

---
+++ tests/cypress/e2e/unit_tests/p0-fleet.spec.ts
@@ -0,0 +1,68 @@
+/*
+Copyright © 2023 - 2024 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import '~/support/commands';
+import * as cypressLib from '@rancher-ecp-qa/cypress-library';
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+
+beforeEach(() => {
+  cy.login();
+  cy.visit('/');
+  cypressLib.burgerMenuToggle();
+  cypressLib.checkNavIcon('cluster-management').should('exist');
+});
+
+
+Cypress.config();
+describe('Fleet Deployment Test Cases', () => {
+
+  qase(62,
+    it('FLEET-62: Deploy application to local cluster', () => {
+      const repoName = "local-cluster-fleet-62"
+      const repoUrl = "https://github.com/rancher/fleet-examples"
+      const branch = "master"
+      const path = "simple"
+
+      // Click on the Continuous Delivery's icon
+      cypressLib.accesMenu('Continuous Delivery');
+      cypressLib.accesMenu('Git Repos');
+
+      // Change namespace to fleet-local
+      cy.contains('fleet-').click();
+      cy.contains('fleet-local').should('be.visible').click();
+
+      // Add Fleet repository and create it
+      cy.addFleetGitRepo(repoName, repoUrl, branch, path);
+      cy.clickButton('Create');
+
+      // Assert repoName exists and its state is 1/1
+      cy.verifyTableRow(0, repoName, '1/1');
+      cy.contains("already exists").should('not.exist');
+
+      // Go to Deploymens in Local Cluster
+      cypressLib.burgerMenuToggle();
+      cypressLib.accesMenu('local');
+      cypressLib.accesMenu('Workloads');
+      cypressLib.accesMenu('Deployments');
+
+      // Verify table rows
+      cy.verifyTableRow(0, 'frontend', '3/3');
+      cy.verifyTableRow(1, 'redis-master', '1/1');
+      cy.verifyTableRow(2, 'redis-slave', '2/2');
+    })
+  );
+
+
+});
+
--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -44,6 +44,7 @@ for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', '
 
 // Add path on "Git Repo:Create"
 Cypress.Commands.add('addPathOnGitRepoCreate', (path) => {
+  cy.clickButton('Add Path');
   cy.get("input[data-testid='input-0'][placeholder='e.g. /directory/in/your/repo']").
     type(path);
 })
@@ -68,6 +69,40 @@ Cypress.Commands.add('addPathOnGitRepoCreate', (path) => {
 //     })
 // })
 
+// Command add Fleet Git Repository
+Cypress.Commands.add('addFleetGitRepo', (repoName, repoUrl, branch, path) => {
+  cy.clickButton('Add Repository');
+  cy.typeValue('Name', repoName);
+  cy.typeValue('Repository URL', repoUrl);
+  cy.typeValue('Branch Name', branch);
+  // Path is not required wwhen git repo contains 1 application folder only.
+  if (path !== null) {
+    cy.addPathOnGitRepoCreate(path);
+  }
+  cy.clickButton('Next');
+})
 
+// Verify  texvalues in table giving the row number
+// More items can be added with new ".and"
+Cypress.Commands.add('verifyTableRow', (rowNumber, expectedText1, expectedText2) => {
+  // Ensure table is loaded and visible
+  // cy.contains('Active',{timeout: 15000}).should('be.visible')
+  cy.contains('tr.main-row[data-testid="sortable-table-0-row"').should('not.be.empty', { timeout: 25000 });
+
+  cy.get(`table > tbody > tr.main-row[data-testid="sortable-table-${rowNumber}-row"]`)
+    .children()
+    .should('contain', expectedText1, { timeout: 30000 })
+    .and('contain', expectedText2,  { timeout: 30000 });
+});
+
+// Namespace Toggle
+Cypress.Commands.add('nameSpaceMenuToggle', (namespaceName) => {
+  cy.get('.top > .ns-filter').should('be.visible')
+  cy.get('.top > .ns-filter').click({force: true});
+  // cy.get('.icon.icon-close').click();
+  cy.get('div.ns-input').should('exist').type(namespaceName);
+  cy.get('.ns-dropdown-menu', { timeout: 5000 }).contains( new RegExp("^" + namespaceName + "$", "g") , { matchCase: true }).should('be.visible').click(); 
+  cy.get('.icon.icon-chevron-up').click({force: true});
+})
 
 
