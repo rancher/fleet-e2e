a831a3a63d501e7cabc1411dea0a311af03b12bf
Manuel Martín <manuel.martin@suse.com>
2024-02-05 14:13:48 +0100

    Removing commenting lines and improving formatting and comments

--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -38,7 +38,7 @@ for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', '
       }, COMMAND_DELAY);
     });
   });
-}; 
+};
 
 // Fleet commands
 
@@ -48,26 +48,6 @@ Cypress.Commands.add('addPathOnGitRepoCreate', (path) => {
   cy.get("input[data-testid='input-0'][placeholder='e.g. /directory/in/your/repo']").
     type(path);
 })
-
-// WIP To be improved
-// It should return value given a coluname and another row parameter.
-// It should be possible find multiple columnames and rowvalues.
-// Cypress.Commands.add('searchInTable', (columnName, textToMatch) => {
-//   cy.get("table > thead > tr > th")
-//     .each(($e1, index, $) => { //iterating through array of elements
-//       const StoreText = $e1.text().trim(); //storing iterated element in text
-//       if (StoreText.includes(columnName)) {
-//         //If text found, iteration stops and checks textToMatch is present
-//         cy.get('tbody tr td')
-//           .eq(index)
-//           .then(($td) => {
-//             const Assigneetext = $td.text().trim();
-//             // cy.wrap(`Column "${columnName}" value is ` + Assigneetext, { timeout: 25000 }).should('contain', textToMatch);
-//             cy.wrap(`Column "${columnName}" value is ` + Assigneetext, { timeout: 25000 }).should('contain', textToMatch);
-//           });
-//       }
-//     })
-// })
 
 // Command add Fleet Git Repository
 Cypress.Commands.add('addFleetGitRepo', (repoName, repoUrl, branch, path) => {
@@ -86,23 +66,20 @@ Cypress.Commands.add('addFleetGitRepo', (repoName, repoUrl, branch, path) => {
 // More items can be added with new ".and"
 Cypress.Commands.add('verifyTableRow', (rowNumber, expectedText1, expectedText2) => {
   // Ensure table is loaded and visible
-  // cy.contains('Active',{timeout: 15000}).should('be.visible')
   cy.contains('tr.main-row[data-testid="sortable-table-0-row"').should('not.be.empty', { timeout: 25000 });
-
   cy.get(`table > tbody > tr.main-row[data-testid="sortable-table-${rowNumber}-row"]`)
     .children()
     .should('contain', expectedText1, { timeout: 30000 })
-    .and('contain', expectedText2,  { timeout: 30000 });
+    .and('contain', expectedText2, { timeout: 30000 });
 });
 
 // Namespace Toggle
 Cypress.Commands.add('nameSpaceMenuToggle', (namespaceName) => {
-  cy.get('.top > .ns-filter').should('be.visible')
-  cy.get('.top > .ns-filter').click({force: true});
-  // cy.get('.icon.icon-close').click();
+  cy.get('.top > .ns-filter').should('be.visible');
+  cy.get('.top > .ns-filter').click({ force: true });
+  // Typing in filter for better targetting the namespece
   cy.get('div.ns-input').should('exist').type(namespaceName);
-  cy.get('.ns-dropdown-menu', { timeout: 5000 }).contains( new RegExp("^" + namespaceName + "$", "g") , { matchCase: true }).should('be.visible').click(); 
-  cy.get('.icon.icon-chevron-up').click({force: true});
+  cy.get('.ns-dropdown-menu', { timeout: 5000 }).contains(new RegExp("^" + namespaceName + "$", "g"), { matchCase: true }).should('be.visible').click();
+  cy.get('.icon.icon-chevron-up').click({ force: true });
 })
-
 
