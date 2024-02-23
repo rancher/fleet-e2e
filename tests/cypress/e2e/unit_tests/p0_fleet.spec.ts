5d61d5248bcf2002e70555721f65df0612a2229b
Manuel Martín <manuel.martin@suse.com>
2024-02-23 09:07:05 +0100

    Increasing timeout to check deployment works to 10 minutes

--- tests/cypress/e2e/unit_tests/p0_fleet.spec.ts
+++ tests/cypress/e2e/unit_tests/p0_fleet.spec.ts
@@ -103,8 +103,8 @@ describe('Fleet Deployment Test Cases', () => {
       cy.wait(2000)
       cy.contains(repoName).click()
       cy.get('.primaryheader > h1').contains(repoName).should('be.visible')
-      cy.get('div.fleet-status', { timeout: 30000 }).eq(0).contains(' 1 / 1 Bundles ready ').should('be.visible')
-      cy.get('div.fleet-status', { timeout: 30000 }).eq(1).contains(' 1 / 1 Resources ready ').should('be.visible')
+      cy.get('div.fleet-status', { timeout: 600000 }).eq(0).contains(' 1 / 1 Bundles ready ').should('be.visible')
+      cy.get('div.fleet-status', { timeout: 600000 }).eq(1).contains(' 1 / 1 Resources ready ').should('be.visible')
       
       // Delete created repo
       cypressLib.burgerMenuToggle();
