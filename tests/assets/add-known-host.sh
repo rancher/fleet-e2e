c3694df7c17ad7a4a403c8c1f74061a80a467044
Manuel Martín <manuel.martin@suse.com>
2024-09-19 09:59:36 +0200

    Improving script to self generate known_host. Improving yaml

--- tests/assets/add-known-host.sh
+++ tests/assets/add-known-host.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+export KNOWN_HOSTS=$(ssh-keyscan -H github.com 2>/dev/null)
+
 # Get yq tool and install it
 echo "Downloading yq tool and installing it"
 wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
@@ -10,4 +12,4 @@ echo "Adding private key"
 yq eval ".stringData.ssh-privatekey = strenv(RSA_PRIVATE_KEY_QA)" -i assets/known-host.yaml
 
 echo "Adding known host key"
-yq eval ".stringData.known_hosts = strenv(RSA_KNOWN_HOSTS_KEY_QA)" -i assets/known-host.yaml
+yq eval ".stringData.known_hosts = strenv(KNOWN_HOSTS)" -i assets/known-host.yaml
