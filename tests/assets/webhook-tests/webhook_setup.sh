183fcf2e8269ae3f0b29c2dffc4447232b6efada
Manuel Martín <manuel.martin@suse.com>
2024-11-14 08:25:15 +0100

    Substituting all curl commands

--- tests/assets/webhook-tests/webhook_setup.sh
+++ tests/assets/webhook-tests/webhook_setup.sh
@@ -11,12 +11,7 @@ export REPO_OWNER="fleetqa"
 export REPO_NAME="webhook-github-test"
 export SECRET_VALUE="webhooksecretvalue"
 # Get the external IP of the Google Cloud instance
-## Troubleshooting: see if the curl command responds:
-wget --quiet --header="Metadata-Flavor: Google" -O - http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip ## Delete this when done
-
-
 export EXTERNAL_IP=$(wget --quiet --header="Metadata-Flavor: Google" -O - http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip) 
-sleep 5
 echo "External IP: ${EXTERNAL_IP}"
 
 # Confirm the external IP is not empty
@@ -27,26 +22,25 @@ fi
 
 # Delete any previous webhook
 # 1 - Get all webhooks
-webhooks=$(curl -s -X GET \
-  https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/hooks \
-  -H 'authorization: Bearer '${GH_PRIVATE_PWD})
+webhooks=$(wget --quiet --header="Authorization: Bearer ${GH_PRIVATE_PWD}" \
+ -O - https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/hooks)
 
 # 2- Extract webhook IDs and delete each one
 echo $webhooks | jq -r '.[].id' | while read webhook_id; do
   echo "Deleting webhook ID: $webhook_id"
-  curl -s -X DELETE \
-    https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/hooks/${webhook_id} \
-    -H 'authorization: Bearer '${GH_PRIVATE_PWD}
+  wget --quiet --method=DELETE --header="Authorization: Bearer ${GH_PRIVATE_PWD}" \
+  -O - https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/hooks/${webhook_id}
 done
 
 echo "All webhooks deleted."
 
 # Create new adhock webhook with the specific Google External IP
-curl -X POST \
-  https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/hooks \
-  -H 'authorization: Bearer '${GH_PRIVATE_PWD} \
-  -H 'content-type: application/json' \
-  -d '{"name":"web","active":true,"events":["push"],"config":{"url":"https://'"${EXTERNAL_IP}"'.nip.io/","content_type":"json","secret":"'"${SECRET_VALUE}"'","insecure_ssl":"1"}}'
+wget --quiet \
+     --method=POST \
+     --header="Authorization: Bearer ${GH_PRIVATE_PWD}" \
+     --header="Content-Type: application/json" \
+     --body-data='{"name":"web","active":true,"events":["push"],"config":{"url":"https://'"${EXTERNAL_IP}"'.nip.io/","content_type":"json","secret":"'"${SECRET_VALUE}"'","insecure_ssl":"1"}}' \
+     -O - https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/hooks
 
 # Add webhook secret 
 kubectl create secret generic gitjob-webhook -n cattle-fleet-system --from-literal=github=$SECRET_VALUE
@@ -73,11 +67,11 @@ spec:
                 number: 80
 EOF
 
-echo "\$PWD" # echo the current path
+echo "$PWD" # echo the current path
 kubectl apply -f webhook-ingress.yaml
 
 # Validate the ingress response (manually use: curl -kv https://${EXTERNAL_IP}.nip.io)
-HTTP_STATUS=$(curl -o /dev/null -k -s -w "%{http_code}" https://${EXTERNAL_IP}.nip.io)
+HTTP_STATUS=$(wget --server-response --spider --no-check-certificate https://${EXTERNAL_IP}.nip.io 2>&1 | awk '/^  HTTP/{print $2}')
 if [ "$HTTP_STATUS" -eq 200 ]; then
     echo "## Webhook ingress correctly created. All good! ##"
 else
