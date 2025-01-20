292aed863553e32f21b577ff7a268d4ce74934a6
Manuel Martin <manuel.martin@suse.com>
2025-01-20 13:06:19 +0100

    extra addition to reduce memory usage

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -39,7 +39,22 @@ export default defineConfig({
           console.log(message)
           return null
         },
-      })  
+      }),
+      // Help for memeory issues.
+      // Ref: https://www.bigbinary.com/blog/how-we-fixed-the-cypress-out-of-memory-error-in-chromium-browsers
+      on("before:browser:launch", (browser, launchOptions) => {
+        
+        launchOptions.args.push("--js-flags=--max-old-space-size=3500");
+        if (["chrome", "edge", "electron"].includes(browser.name)) {
+          if (browser.isHeadless) {
+            launchOptions.args.push("--no-sandbox");
+            launchOptions.args.push("--disable-gl-drawing-for-tests");
+            launchOptions.args.push("--disable-gpu");
+          }
+        }
+        return launchOptions;
+      });  
+
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       require('cypress/plugins/index.ts')(on, config)
       // eslint-disable-next-line @typescript-eslint/no-var-requires
