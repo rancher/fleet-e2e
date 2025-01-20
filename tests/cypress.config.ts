758989369782c5cf86985ef8a4afe902403e7717
Manuel Martin <manuel.martin@suse.com>
2025-01-20 16:07:55 +0100

    Adding chrome as default browser. Other refactors to conflicting with Electron

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -5,6 +5,7 @@ const qaseAPIToken = process.env.QASE_API_TOKEN
 export default defineConfig({
   viewportWidth: 1314,
   viewportHeight: 954,
+  defaultBrowser: 'chrome',
   defaultCommandTimeout: 10000,
   video: true,
   videoCompression: true,
@@ -44,17 +45,16 @@ export default defineConfig({
       // Ref: https://www.bigbinary.com/blog/how-we-fixed-the-cypress-out-of-memory-error-in-chromium-browsers
       on("before:browser:launch", (browser, launchOptions) => {
         
-        launchOptions.args.push("--js-flags=--max-old-space-size=3500");
-        if (["chrome", "edge", "electron"].includes(browser.name)) {
+        if (["chrome", "edge"].includes(browser.name)) {
           if (browser.isHeadless) {
             launchOptions.args.push("--no-sandbox");
             launchOptions.args.push("--disable-gl-drawing-for-tests");
             launchOptions.args.push("--disable-gpu");
           }
+          launchOptions.args.push("--js-flags=--max-old-space-size=3500");
         }
         return launchOptions;
       });  
-
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       require('cypress/plugins/index.ts')(on, config)
       // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -62,5 +62,6 @@ export default defineConfig({
       return config;
     },
     specPattern: 'cypress/e2e/unit_tests/*.spec.ts',
+    
   },
 })
