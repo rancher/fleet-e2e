0eec41ae15fd5f27232073b1cbd0da57d12f8180
Manuel Martin <manuel.martin@suse.com>
2025-09-02 12:27:03 +0200

    Using solely PUBLIC DNS as variable for GCP

--- tests/scripts/certs-and-rancher-install.sh
+++ tests/scripts/certs-and-rancher-install.sh
@@ -15,14 +15,8 @@ helm upgrade --install cert-manager --namespace cert-manager jetstack/cert-manag
 
 sleep 8
 
-# # Getting Google IP
-# export GCP_EXTRENAL_IP=$(wget --quiet --header="Metadata-Flavor: Google" -O - http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip) 
-
-# Rancher installation
-export MY_IP=${PUBLIC_DNS}
-export SYSTEM_DOMAIN="${MY_IP}.bc.googleusercontent.com"
+# Rancher installation (using PUBLIC_DNS as hostanme for GCP)
 export RANCHER_USER=admin RANCHER_PASSWORD=rancherpassword
-export RANCHER_URL=https://${MY_IP}.bc.googleusercontent.com/dashboard
 
 
 helm repo add rancher-head-2.12 https://charts.optimus.rancher.io/server-charts/release-2.12
@@ -31,7 +25,7 @@ helm repo update
 helm upgrade --install rancher rancher-head-2.12/rancher \
   --devel \
   --namespace cattle-system --create-namespace \
-  --set hostname=$SYSTEM_DOMAIN \
+  --set hostname=$PUBLIC_DNS \
   --set bootstrapPassword=rancherpassword \
   --set replicas=1 \
  \
