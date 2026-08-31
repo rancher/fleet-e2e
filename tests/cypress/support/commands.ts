4acf1008f3f302e9971521e471f81887c6077ae3
Satyajit Bulage <satyajit.bulage@suse.com>
2026-08-31 14:00:04 +0530

    [2.16] Fix search box locator to include additional input types for filtering

--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -575,10 +575,10 @@ Cypress.Commands.add('nameSpaceMenuToggle', (namespaceName) => {
 
 // Command to filter text in searchbox
 Cypress.Commands.add('filterInSearchBox', (filterText) => {
-  cy.get('input.input-sm.search-box').should('be.visible');
+  cy.get('input.input-sm.search-box,.labeled-input.edit.compact-input.search-box').should('be.visible');
   // Added 1/2 seconds of wait, as element is hidden after it gets visible.
   cy.wait(500);
-  cy.get('input.input-sm.search-box').clear().type(filterText);
+  cy.get('input.input-sm.search-box,.labeled-input.edit.compact-input.search-box').clear().type(filterText);
   cy.wait(250); // Adding 1/4 second to ensure next action is executed more reliably
 });
 
