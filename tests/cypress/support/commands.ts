5200ddb38a0aded2a88d3938e0b36abdc7083c32
Manuel Martin <manuel.martin@suse.com>
2026-09-04 12:24:04 +0200

    Adpating locator for visibility in 2.16

--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -1467,7 +1467,12 @@ Cypress.Commands.add('checkAccessToCreateGitRepoPage', () => {
     cy.get('[data-testid="subtype-banner-item-fleet.cattle.io.gitrepo"]')
       .should('be.visible')
       .trigger('mouseenter', { force: true });
-    cy.contains('You have no permissions to create Git Repos').should('be.visible');
+
+    // Adapted locator after 2.16.
+    // Explicit call to visibility not working well.
+    cy.contains('You have no permissions to create Git Repos').should('exist');
+    cy.get('*:visible:contains("You have no permissions to create Git Repos")').should('exist');
+
     cy.clickButton('Cancel');
   } else {
     cy.get('.btn.role-primary').contains('Add Repository').should('not.exist');
